605cedf066fd4908b7f2e537031c538e6e88ccee
KenzieFu <kenziefubrianto@gmail.com>
2024-06-20 20:15:23 +0700

    env

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 USER = os.getenv('GOOGLE_CLOUD_SQL_USER','root') 
 PASS = os.getenv('GOOGLE_CLOUD_PASS','') 
-HOST = os.getenv('GOOGLE_CLOUD_HOST','localhost') 
+HOST = os.getenv('GOOGLE_CLOUD_HOST','localhost')  
 DBNAME = os.getenv('GOOGLE_CLOUD_DATABASE','backend-asn') 
 
 # URL_DATABASE = 'mysql+pymysql://asn:3Ha&*|RCQm|~Pp/X@34.101.41.205/backend-asn'
